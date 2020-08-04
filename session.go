028a36e65d8b21481ed9bf4b197ee03e10df4d74
Dings <deen.job@qq.com>
2020-08-04 23:46:31 +0800

    branch dev.

--- session.go
+++ session.go
@@ -1,75 +1,116 @@
 // Copyright (c) 2020 HigKer
 // Open Source: MIT License
 // Author: SDing <deen.job@qq.com>
-// Date: 2020/7/24 - 07:47 PM
+// Date: 2020/8/4 - 10:40 PM
 
 package session
 
 import (
 	"errors"
 	"net/http"
-	"net/url"
+	"sync"
 	"time"
 )
 
-var (
-	_Store     Store
-	CookieName = "go_session_key"
-	MaxAge     = 60 * 30 // 30 min
+type StoreType int8
+
+const (
+	MemoryStore StoreType = iota
+	RedisStore
+	maxSize = 16
 )
 
-// Session Operation interface, Session operation of different storage methods is different,
-// and the implementation is also different.
+var (
+	_Store            Store
+	DefaultCookieName = "go_session_key"
+	DefaultMaxAge     = 60 * 30 // 30 min
+)
+
+// Session standard
 type Session interface {
-	Set(key, value interface{})
-	Get(key interface{}) interface{}
-	Remove(key interface{}) error
-	GetID() string
+	Set(Key, value interface{})
+	Get(Key interface{}) interface{}
+	Remove(Key interface{})
+	ID() string
+	Clear()
 }
 
-// Builder build session
-func Builder(storeType Store) error {
-	switch storeType.(type) {
-	default:
-		return errors.New("not implement store type")
-	case *MemoryStore:
-		_Store = storeType
-		go storeType.(*MemoryStore).GC()
+// Config param
+type Config struct {
+	// cookie参数
+	CookieName string // sessionID的cookie键名
+	Domain     string // sessionID的cookie作用域名
+	Path       string // sessionID的cookie作用路径
+	//Key                       string        // sessionID值加密的密钥
+	RedisAddr                 string        // redis地址
+	RedisPassword             string        // redis密码
+	RedisKeyPrefix            string        // redis键名前缀
+	MaxAge                    int           // 最大生命周期（秒）
+	IdleTime                  time.Duration // 空闲生命周期
+	RedisDB                   int           // redis数据库
+	HttpOnly                  bool          // 仅用于http（无法被js读取）
+	Secure                    bool          // 启用https
+	DisableAutoUpdateIdleTime bool          // 禁止自动更新空闲时间
+}
+
+// Item a session  item
+type Item struct {
+	SID              string                      // unique id
+	Safe             sync.Mutex                  // mutex lock
+	LastAccessedTime time.Time                   // last visit time
+	MaxAge           int64                       // over time
+	Data             map[interface{}]interface{} // save data
+}
+
+//实例化
+func newSessionItem(id string) *Item {
+	return &Item{
+		Data: make(map[interface{}]interface{}, maxSize),
+		SID:  id,
+	}
+}
+
+//同一个会话均可调用，进行设置，改操作必须拥有排斥锁
+func (si *Item) Set(key, value interface{}) {
+	si.Safe.Lock()
+	defer si.Safe.Unlock()
+	si.Data[key] = value
+}
+
+func (si *Item) Get(key interface{}) interface{} {
+	if value := si.Data[key]; value != nil {
+		return value
 	}
 	return nil
 }
 
-func Handle(w http.ResponseWriter, r *http.Request) (Session, error) {
-	//防止处理时，进入另外的请求
-	cookie, err := r.Cookie(CookieName)
-	if err != nil || len(cookie.Value) <= 0 {
-		_, item := setCookie(w, cookie)
-		return item, nil
+func (si *Item) Remove(key interface{}) {
+	if value := si.Data[key]; value != nil {
+		delete(si.Data, key)
 	}
-	// 防止浏览器关闭重新打开抛异常
-	sid, _ := url.QueryUnescape(cookie.Value)
-	session := _Store.(*MemoryStore).sessions[sid]
-	if session == nil {
-		_, item := setCookie(w, cookie)
-		return item, nil
-	}
-	return _Store.(*MemoryStore).sessions[cookie.Value], nil
 }
 
-func setCookie(w http.ResponseWriter, cookie *http.Cookie) (*http.Cookie, *MemoryItem) {
-	// 创建一个cookie
-	sid := string(Random(32, RuleKindAll))
-	cookie = &http.Cookie{
-		Name: CookieName,
-		//这里是并发不安全的，但是这个方法已上锁
-		Value:    url.QueryEscape(sid), //转义特殊符号@#￥%+*-等
-		Path:     "/",
-		HttpOnly: true,
-		MaxAge:   MaxAge,
-		Expires:  time.Now().Add(time.Duration(MaxAge)),
+func (si *Item) ID() string {
+	return si.SID
+}
+func (si *Item) Clear() {
+
+}
+
+func Builder(store StoreType, conf *Config) (Session, error) {
+	switch store {
+	default:
+		return nil, errors.New("build session error, not implement type store")
+	case MemoryStore:
+
+	case RedisStore:
+
 	}
-	http.SetCookie(w, cookie) //设置到响应中
-	item := newMemoryItem(sid)
-	_Store.(*MemoryStore).sessions[sid] = item
-	return cookie, item
+	return nil, nil
+}
+
+// Action Session handle func
+func Action(w http.ResponseWriter, r *http.Request) (Session, error) {
+
+	return nil, nil
 }
