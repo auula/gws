cc562633903da23525fec7efc3eeca03f366a81d
Jarvib Ding <deen.job@qq.com>
2021-03-20 20:20:34 +0800

    fix: error process

--- session.go
+++ session.go
@@ -1,5 +1,68 @@
 package sessionx
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	mux sync.Mutex
+	mgr *manager
+)
+
 type Session struct {
-	store Storage
+	ID      string
+	Expires time.Time
+	Data    map[string]interface{}
 }
+
+func (s *Session) Get(key string) (interface{}, error) {
+	if len(key) <= 0 {
+		return nil, fmt.Errorf("key '%s' invalid", key)
+	}
+	bytes, err := mgr.store.Reader(s)
+	if err != nil {
+		return nil, err
+	}
+	Decoder(bytes, s)
+	if ele, ok := s.Data[key]; ok {
+		return ele, nil
+	}
+	return nil, fmt.Errorf("key '%s' does not exits", key)
+}
+
+func (s *Session) Set(key string, v interface{}) error {
+
+	return nil
+}
+
+func (s *Session) Remove() {
+
+}
+
+// async code
+
+// type Session struct {
+// 	ID      string
+// 	Expires time.Time
+// 	Data    map[string]interface{}
+// 	// 错误一次性收集
+// 	tasks []task
+// 	collgroup.Group
+// }
+
+// func (s *Session) Get(key string) interface{} {
+// 	s.tasks = append(s.tasks, func() error {
+// 		bytes, err := mgr.store.Reader(s)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		Decoder(bytes, s)
+// 		return nil
+// 	})
+// 	if ele, ok := s.Data[key]; ok {
+// 		return ele
+// 	}
+// 	return nil
+// }
