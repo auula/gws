bac0562d8f765ef555911d8f294a1b170f36a890
Dings <deen.job@qq.com>
2021-03-27 13:06:31 +0800

    add: test coverage shell script

--- .deepsource.sh
+++ .deepsource.sh
@@ -8,7 +8,7 @@ go test -coverprofile=cover.out
 curl https://deepsource.io/cli | sh
 
 # Set DEEPSOURCE_DSN env variable from repository settings page
-export DEEPSOURCE_DSN=$secrets.CODECOV_TOKEN
+export DEEPSOURCE_DSN=$DEEPSOURCE_DSN
 
 # From the root directory, run the report coverage command
 ./bin/deepsource report --analyzer test-coverage --key go --value-file ./cover.out
