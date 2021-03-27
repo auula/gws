9c5b586b7942a3d07722326c9696664a4f88ce00
Jarvib Ding <deen.job@qq.com>
2021-03-27 12:44:40 +0800

    Create deepsource.sh

---
+++ deepsource.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "deepsource.sh running...."
+
+# Run your tests and generate coverage report
+go test -coverprofile=cover.out
+
+# Install 'deepsource CLI'
+curl https://deepsource.io/cli | sh
+
+# Set DEEPSOURCE_DSN env variable from repository settings page
+export DEEPSOURCE_DSN=${{ secrets.CODECOV_TOKEN }}
+
+# From the root directory, run the report coverage command
+./bin/deepsource report --analyzer test-coverage --key go --value-file ./cover.out
